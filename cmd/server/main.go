99310f272475d5c927130d179a6f1275c1207f10
Alain <alain.devcs@gmail.com>
2023-12-22 14:26:12 +0100

    testing write

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"io"
 	"net"
 	"os"
 	"os/signal"
@@ -27,27 +27,94 @@ func handleSignals(closer func()) {
 	}()
 }
 
+// Receive everything from the client until connection gets closed
+// func handleClient(conn net.Conn) {
+// 	defer conn.Close()
+
+// 	// Set timeout on r/w operations
+// 	conn.SetDeadline(time.Now().Add(RW_TIMEOUT))
+
+// 	buf, err := io.ReadAll(conn)
+// 	if err != nil && len(buf) == 0 {
+// 		fmt.Println("Error reading from connection:", err.Error())
+// 		return
+// 	}
+
+// 	fmt.Printf("Received: \"%s\"\n", string(buf))
+
+// 	// Write data back to the client
+// 	_, err = conn.Write(buf)
+// 	if err != nil {
+// 		fmt.Println("Error:", err)
+// 		return
+// 	}
+// }
+
+// It works but gets stuck on write side if larger than X bytes
+// func handleClient(conn net.Conn) {
+//  defer conn.Close()
+// 	chunk := make([]byte, RECV_CHUNK_SIZE)
+
+// 	for {
+// 		len, err := conn.Read(chunk)
+
+// 		if err != nil {
+// 			fmt.Println("Error reading:", err.Error())
+// 			break
+// 		}
+
+// 		s := string(chunk[:len])
+
+// 		fmt.Println("Stuff", s)
+// 		fmt.Println("len", binary.Size(chunk))
+// 	}
+// }
+
 func handleClient(conn net.Conn) {
 	defer conn.Close()
 
-	// Set timeout on r/w operations
-	conn.SetDeadline(time.Now().Add(RW_TIMEOUT))
+	var received int
+	// The buffer grows as we write into it.
+	// Ref: https://pkg.go.dev/bytes#Buffer
+	buffer := bytes.NewBuffer(nil)
 
-	buf, err := io.ReadAll(conn)
-	if err != nil && len(buf) == 0 {
-		fmt.Println("Error reading from connection:", err.Error())
-		return
+	// Read the data in chunks.
+	for {
+		// e.g. 8192
+		chunk := make([]byte, RECV_CHUNK_SIZE)
+		read, err := conn.Read(chunk)
+		if err != nil {
+			fmt.Println(received, buffer.Bytes(), err)
+			return
+		}
+		received += read
+		buffer.Write(chunk[:read])
+
+		if read == 0 || read < RECV_CHUNK_SIZE {
+			break
+		}
 	}
 
-	fmt.Printf("Received: \"%s\"\n", string(buf))
-
-	// Write data back to the client
-	_, err = conn.Write(buf)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
+	conn.Write([]byte("OK\n"))
+	fmt.Println("Recvd:", received, buffer.String())
 }
+
+// func handleClient2(conn net.Conn) {
+// 	defer conn.Close()
+
+// 	// Wrap connection with buffered reader so size gets handled automatically
+// 	r := bufio.NewReader(conn)
+
+// 	for {
+// 		// s, err := r.ReadString('\n')
+// 		s, err := r.ReadBytes('\n')
+// 		if err != nil {
+// 			fmt.Println("Error reading:", err.Error())
+// 			break
+// 		}
+// 		fmt.Println("received:", s)
+// 	}
+// }
 
 func main() {
 	// Start TCP server
@@ -62,7 +129,10 @@ func main() {
 		listener.Close()
 	}
 
+	// If killed by SIGTERM
 	handleSignals(closer)
+	// If died natually
+	defer closer()
 
 	fmt.Println("Server is listening on", ADDR)
 
