e763bbeb6c5829c81a2e42eb6a51234cab6a09a9
Alain <alain.devcs@gmail.com>
2023-12-22 13:28:35 +0100

    closing handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
 	"net"
 	"os"
 	"os/signal"
@@ -16,13 +16,14 @@ var RECV_BUF_SIZE = 4096
 var RECV_CHUNK_SIZE = 32
 
 // handleSignals registers signal handlers for shutdown
-func handleSignals() {
+func handleSignals(closer func()) {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
 		fmt.Println("received SIGTERM, exiting")
-		os.Exit(1)
+		closer()
+		os.Exit(0)
 	}()
 }
 
@@ -32,31 +33,16 @@ func handleClient(conn net.Conn) {
 	// Set timeout on r/w operations
 	conn.SetDeadline(time.Now().Add(RW_TIMEOUT))
 
-	// Read and process data from the client looping through a buffer of 1024 bytes
-	var recvd int
-	buf := bytes.NewBuffer(nil)
-
-	for {
-		chunk := make([]byte, RECV_CHUNK_SIZE)
-		n, err := conn.Read(chunk)
-
-		if err != nil {
-			fmt.Printf("Err reading chunk: \"%s\"", string(chunk[:n]))
-			return
-		}
-
-		recvd += n
-		buf.Write(chunk[:n])
-
-		if recvd == 0 || recvd < RECV_CHUNK_SIZE {
-			break
-		}
+	buf, err := io.ReadAll(conn)
+	if err != nil && len(buf) == 0 {
+		fmt.Println("Error reading from connection:", err.Error())
+		return
 	}
 
-	fmt.Printf("Received: \"%s\"\n", buf.String())
+	fmt.Printf("Received: \"%s\"\n", string(buf))
 
 	// Write data back to the client
-	_, err := conn.Write(buf.Bytes())
+	_, err = conn.Write(buf)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
@@ -64,15 +50,19 @@ func handleClient(conn net.Conn) {
 }
 
 func main() {
-	handleSignals()
-
 	// Start TCP server
 	listener, err := net.Listen("tcp", ADDR)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
-	defer listener.Close()
+
+	closer := func() {
+		fmt.Println("Closing listener...")
+		listener.Close()
+	}
+
+	handleSignals(closer)
 
 	fmt.Println("Server is listening on", ADDR)
 
