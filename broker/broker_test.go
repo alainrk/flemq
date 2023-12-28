497f7ce143b9fc6c7bd42dd4bd54d3b92fb95aaf
Alain <alain.devcs@gmail.com>
2023-12-28 16:22:50 +0100

    fix test broker

--- broker/broker_test.go
+++ broker/broker_test.go
@@ -7,49 +7,62 @@ import (
 	"time"
 )
 
-func subscriberTest(b *Broker[string], id int, wg *sync.WaitGroup, res chan int) {
+func subscriberTest(b *Broker[string], id int, res chan int) {
 	s := b.Subscribe()
 	i := 0
 	for m := range s {
 		fmt.Printf("Client %d got message: %v\n", id, m)
 		i++
 	}
-	fmt.Printf("Client done")
+
+	fmt.Printf("Client %d done with %d messages\n", id, i)
+
 	res <- i
 	b.Unsubscribe(s)
-	wg.Done()
 }
 
-func publisherTest(b *Broker[string], count int, wg *sync.WaitGroup) {
+func publisherTest(b *Broker[string], count int) {
 	for i := 0; i < count; i++ {
 		b.Publish(fmt.Sprintf("msg#%d", i))
 		time.Sleep(100 * time.Millisecond)
 	}
-	b.Stop()
-	wg.Done()
 }
 
 func TestOneSubscriber(t *testing.T) {
-	var wg sync.WaitGroup
-	res := make(chan int)
+	var (
+		wg  sync.WaitGroup
+		res = make(chan int)
+		b   = NewBroker[string]()
+	)
 
-	b := NewBroker[string]()
 	go b.Start()
 
 	wg.Add(1)
-	go subscriberTest(b, 1, &wg, res)
-
-	// Start publishing messages:
-	wg.Add(1)
-	go publisherTest(b, 5, &wg)
+	go func() {
+		defer wg.Done()
+		subscriberTest(b, 1, res)
+	}()
 
 	wg.Add(1)
 	go func() {
+		defer wg.Done()
+		publisherTest(b, 5)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		time.Sleep(1 * time.Second)
+		b.Stop()
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
 		r := <-res
 		if r != 5 {
 			t.Errorf("Expected 5 messages, got %d", r)
 		}
-		wg.Done()
 	}()
 
 	wg.Wait()
