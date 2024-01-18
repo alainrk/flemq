840e942f8537a41522c76d03c90e41152372e5e0
Alain <alain.devcs@gmail.com>
2024-01-18 07:22:11 +0100

    fix script

--- scripts/gif.sh
+++ scripts/gif.sh
@@ -5,6 +5,6 @@ ffmpeg -i input.mov -pix_fmt rgb8 -r 10 output.gif && gifsicle -O3 output.gif -o
 
 # speed up gif
 # 1x30 is ImageMagick’s notation for 1 times 1/30th of a second
-convert -delay 1x100 flemq.gif output.gif
+convert -delay 1x100 output.gif flemq.gif
 
 exit 0
