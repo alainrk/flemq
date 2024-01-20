1dc79cb08da916bdf2bddaaf9aaa7726f5bd9e53
Alain <alain.devcs@gmail.com>
2024-01-20 14:08:43 +0100

    implementing simple commands in ts

--- example/ts/src/index.ts
+++ example/ts/src/index.ts
@@ -1,17 +1,64 @@
 import * as net from "net";
 
-const client = new net.Socket();
+type FlemQClientOptions = {
+  host?: string;
+  port: number;
+};
 
-client.connect(22123, "127.0.0.1", () => {
-  console.log("Connected to server");
-  client.write("push topicA xxxx");
-});
+class FlemQ {
+  private client: net.Socket;
+  private options: FlemQClientOptions;
 
-client.on("data", (data) => {
-  console.log("Received: " + data);
-  client.destroy();
-});
+  constructor(opt: FlemQClientOptions) {
+    this.client = new net.Socket();
+    this.options = opt;
+  }
 
-client.on("close", () => {
-  console.log("Connection closed");
-});
+  // connect client to server
+  async connect(): Promise<FlemQ> {
+    return new Promise((resolve, reject) => {
+      this.client.connect(
+        this.options.port,
+        this.options.host || "localhost",
+        () => {
+          console.log("Connection established");
+          resolve(this);
+        }
+      );
+
+      this.client.on("error", (err) => {
+        reject(err);
+      });
+    });
+  }
+
+  // send data to server
+  private async send(data: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      this.client.write(data, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve("");
+        }
+      });
+
+      this.client.on("data", (data) => {
+        resolve(data.toString());
+      });
+    });
+  }
+
+  async push(topic: string, data: string): Promise<string> {
+    return this.send(`PUSH ${topic} ${data}`);
+  }
+}
+
+(async () => {
+  const flemq = new FlemQ({
+    port: 22123,
+  });
+
+  await flemq.connect();
+  await flemq.push("x", "hello world");
+})();
